c7983d37af2af0f9192b2f7429fb29e1d66a2bcf
Rangana Maddumahewa <rbasehewa@gmail.com>
2019-09-26 13:57:08 +0930

    add click listeners to square

--- RGB_Color_Game/colorGame.js
+++ RGB_Color_Game/colorGame.js
@@ -11,7 +11,17 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
 
 for(var i = 0; i < squares.length; i++){
     squares[i].style.backgroundColor = colors[i];
+
+    squares[i].addEventListener("click",function(){
+        alert("Clicked a square");
+    })
+
 }
