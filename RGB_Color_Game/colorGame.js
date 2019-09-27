68b58622084484489837d7d9901293aec8e84538
Rangana Maddumahewa <rbasehewa@gmail.com>
2019-09-27 14:25:24 +0930

    updated second and third phases

--- RGB_Color_Game/colorGame.js
+++ RGB_Color_Game/colorGame.js
@@ -11,29 +11,42 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickedColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 
-for(var i = 0; i < squares.length; i++){
+for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
 
-    squares[i].addEventListener("click",function(){
-    
-    var clickedColor = this.style.backgroundColor;
-    
-    if(clickedColor === pickedColor){
-        // alert("Correct!");
-        messageDisplay.textContent = "Correct!";
-    }else{
-        // alert("incorrect color");
-        this.style.backgroundColor =  '#232323';
-        messageDisplay.textContent = "Try Again";
-    }
+    squares[i].addEventListener("click", function () {
+
+        var clickedColor = this.style.backgroundColor;
+
+        if (clickedColor === pickedColor) {
+            // alert("Correct!");
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+        } else {
+            // alert("incorrect color");
+            this.style.backgroundColor = '#232323';
+            messageDisplay.textContent = "Try Again";
+        }
 
     })
 
+}
+
+function changeColors(colors) {
+
+    for (var i = 0; i < squares.length; i++) {
+
+        squares[i].style.backgroundColor = colors;
+    }
+}
+function pickedColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
