c34fd6a97722895ea4ffd0724bb47dbff37374c4
Rangana Maddumahewa <rbasehewa@gmail.com>
2019-10-10 18:48:33 +1030

    updated whole logic

--- RGB_Color_Game/colorGame.js
+++ RGB_Color_Game/colorGame.js
@@ -6,21 +6,26 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickedColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
 
 for (var i = 0; i < squares.length; i++) {
+    //add click listeners to squares
     squares[i].style.backgroundColor = colors[i];
 
+    //grab color of clicked square
     squares[i].addEventListener("click", function () {
-
+    // grab color of clicked square
         var clickedColor = this.style.backgroundColor;
-
+    // compare color to pickedColor
+    console.log(clickedColor, pickedColor);
         if (clickedColor === pickedColor) {
             // alert("Correct!");
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.background = clickedColor;
         } else {
             // alert("incorrect color");
             this.style.backgroundColor = '#232323';
@@ -47,7 +52,7 @@ function generateRandomColors(num){
     // make an array
     var random = [];
     // add random colors to an array
-    for(var i = 0;i < num; i++){
+    for(var i = 0; i < num; i++){
     //get the color and push into arr
 
      random.push(randomColor());
@@ -67,5 +72,5 @@ function randomColor(){
 
     //    "rgb(255, 0, 0)",
 
-    return "rgb(" + r + "," + g + "," + b + ")";
+    return "rgb(" + r + ", "+ g +", " + b + ")";
 }
